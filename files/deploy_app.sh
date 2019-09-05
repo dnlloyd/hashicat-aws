ac9f17104dc235d07bba0571d24a179e082b613a
Dan Lloyd <dnlloyd@outlook.com>
2019-09-05 14:33:50 -0500

    Update deploy_app.sh

--- files/deploy_app.sh
+++ files/deploy_app.sh
@@ -9,7 +9,7 @@ cat << EOM > /var/www/html/index.html
   <div style="width:800px;margin: 0 auto">
 
   <!-- BEGIN -->
-  <center><img src="http://${PLACEHOLDER}/${WIDTH}/${HEIGHT}"></img></center>
+  <center><img src="https://static.boredpanda.com/blog/wp-content/uploads/2018/02/stray-dog-frodo-transformation-marko-duspara-fb__700-png.jpg"></img></center>
   <center><h2>Meow World!</h2></center>
   Welcome to ${PREFIX}'s app. Replace this text with your own. 
   <!-- END -->
